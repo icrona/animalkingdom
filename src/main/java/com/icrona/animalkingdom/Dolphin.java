95e86e434f1053a7d7dbd73a27f08fa0cb282e3c
Aldo Makmur <aldo@hapz.com>
2020-06-22 00:50:14 +0800

    SectionB3: dolphin is not a fish,  but is a swimmer

---
+++ src/main/java/com/icrona/animalkingdom/Dolphin.java
@@ -0,0 +1,9 @@
+package com.icrona.animalkingdom;
+
+public class Dolphin implements Swimmer {
+
+    @Override
+    public void swim() {
+        System.out.println("Dolphin is swimming");
+    }
+}
